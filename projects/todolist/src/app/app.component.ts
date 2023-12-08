1d409f7f5b2147f9e78eec30b59fe2a1ac0783b2
Ismail Bennine <ismail.bennine@univ-lille.fr>
2023-12-08 08:53:46 +0100

    commit 2

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: 
-  '<h1>Hello world !<h1>',
+  template: `
+  <h1>La liste des choses a faire <h1>
+
+  <ul *ngFor="let todo of todoList">
+      <li *ngIf="!todo.isCompleted">{{ todo.title }}</li>
+  </ul>
+  `,
   styles: []
 })
 export class AppComponent {
+
+  todoList = TODOS;
+
+  ngOnInit(){
+    console.table(this.todoList);
+  }
+  
 }
